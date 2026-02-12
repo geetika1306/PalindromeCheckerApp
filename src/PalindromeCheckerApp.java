e761341f55458b3b50e9fafffd0a3f62b0d71029
geetika1306 <geetika25117@gmail.com>
2026-02-12 16:11:08 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+/*
+UC1 PALINDROME CHECKER APP;
+Description:
+This class displays the entry point of the
+Palindrome Checker Management System.
+AUTHOR: Geetika k
+ */
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Successfully.");
+    }
+}
